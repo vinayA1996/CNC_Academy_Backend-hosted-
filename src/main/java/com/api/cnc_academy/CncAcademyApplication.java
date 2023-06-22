67ca64e689fd98f1db2cd8cad6effad49208538c
vinayA1996 <126498393+vinayA1996@users.noreply.github.com>
2023-06-22 19:36:30 +0530

    updated Cros origin

--- src/main/java/com/api/cnc_academy/CncAcademyApplication.java
+++ src/main/java/com/api/cnc_academy/CncAcademyApplication.java
@@ -3,7 +3,13 @@ package com.api.cnc_academy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class CncAcademyApplication {
@@ -12,5 +18,20 @@ public class CncAcademyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CncAcademyApplication.class, args);
 	}
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://localhost:4200");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
 
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**",config);
+
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+		return bean;
+	}
 }
