19c1cf03133c9083a72bdbf71d73a1818b296900
vinayA1996 <126498393+vinayA1996@users.noreply.github.com>
2023-06-22 12:43:41 +0530

    properties chnaged

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@
 #--------------------------------------
 FROM maven:3.6.0-jdk-11-slim AS build
 COPY . .
-RUN mvn clean package -Pprod -DskipTests
+RUN mvn clean package
 #
 # Package stage
 #
--- src/main/java/com/api/cnc_academy/CncAcademyApplication.java
+++ src/main/java/com/api/cnc_academy/CncAcademyApplication.java
@@ -1,10 +1,14 @@
 package com.api.cnc_academy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class CncAcademyApplication {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CncAcademyApplication.class, args);
